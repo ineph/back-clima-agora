fe5f3ffc6284c77ea786cd73cbd6b64f92537805
lucas <lcs999999999@hotmail.com>
2020-09-11 23:45:20 -0300

    first responsive route

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 import requests
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 weatherbit_api = 'http://api.weatherbit.io/v2.0/forecast/daily'
 weatherbit_api_key = '55122f2c18204337a2a823a4e91778fa'
-# response = requests.get(base_api_url + '?city=Raleigh,NC&lang=pt&days=7&key=' + api_key)
-
-openweather_api_key = 'db30105f4c7958ae5633b69b98dce69f'
-response = 'http://api.openweathermap.org/data/2.5/weather?q={Aracatuba}&appid={' + openweather_api_key + '}'
-testemap = 'http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?date=1527811200&opacity=0.9&fill_bound=true&appid={db30105f4c7958ae5633b69b98dce69f}'
 
 def jsonprint(obj):
     text = json.dumps(obj,sort_keys= True, indent= 4)
     return(text)
 
 @app.route("/")
-def dummy_api():
-    show = testemap.json()
+def call_api():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    response = requests.get(weatherbit_api + '?lat='+lat+'&lon='+lon+'&lang=pt&days=7&key=' + weatherbit_api_key)
+    show = response.json()
     return(show)
 
 
