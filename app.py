d4e70c7ee8493337d81bbc76c0599a3e5475b40f
lucas <lcs999999999@hotmail.com>
2020-09-13 00:47:02 -0300

    added /current route and specific varaibles

--- app.py
+++ app.py
@@ -6,18 +6,28 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-weatherbit_api = 'http://api.weatherbit.io/v2.0/forecast/daily'
+weatherbit_api_base = 'http://api.weatherbit.io/v2.0/'
 weatherbit_api_key = '55122f2c18204337a2a823a4e91778fa'
+forecast_weather = 'forecast/daily'
+current_weather = 'current'
 
 def jsonprint(obj):
     text = json.dumps(obj,sort_keys= True, indent= 4)
     return(text)
 
-@app.route("/")
-def call_api():
+@app.route("/forecast")
+def get_forecast():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
-    response = requests.get(weatherbit_api + '?lat='+lat+'&lon='+lon+'&lang=pt&days=7&key=' + weatherbit_api_key)
+    response = requests.get(weatherbit_api_base + forecast_weather + '?lat='+lat+'&lon='+lon+'&lang=pt&days=7&key=' + weatherbit_api_key)
+    show = response.json()
+    return(show)
+
+@app.route("/current")
+def get_current():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    response = requests.get(weatherbit_api_base + current_weather + '?lat='+lat+'&lon='+lon+'&lang=pt&days=7&key=' + weatherbit_api_key)
     show = response.json()
     return(show)
 
