6fe31e7ed3c294d759899ef7a614574ade76ce0b
lucas <lcs999999999@hotmail.com>
2020-09-14 23:04:35 -0300

    correct error line 19

--- app.py
+++ app.py
@@ -12,25 +12,35 @@ weatherbit_api_key = '55122f2c18204337a2a823a4e91778fa'
 forecast_weather = 'forecast/daily'
 current_weather = 'current'
 
-# def jsonprint(obj):
-#     text = json.dumps(obj,sort_keys= True, indent= 4)
-#     return(text)
 
 @app.route("/forecast")
 def get_forecast():
-    lat = request.args.get('lat')
-    lon = request.args.get('lon')
-    response = requests.get(weatherbit_api_base + forecast_weather + '?lat='+lat+'&lon='+lon+'&lang=pt&days=7&key=' + weatherbit_api_key)
-    show = response.json()
-    return(show)
+    if 'city' in request.headers:
+        place = request.args('city')
+        response = requests.get(weatherbit_api_base + forecast_weather + '?city=' + place + '&lang=pt&days=7&key=' + weatherbit_api_key)
+        show = response.json()
+        return(show)
+    else:
+        print('sexo anal')
+        lat = request.args.get('lat')
+        lon = request.args.get('lon')
+        response = requests.get(weatherbit_api_base + forecast_weather + '?lat=' + lat + '&lon=' + lon + '&lang=pt&days=7&key=' + weatherbit_api_key)
+        show = response.json()
+        return(show)
 
 @app.route("/current")
 def get_current():
-    lat = request.args.get('lat')
-    lon = request.args.get('lon')
-    response = requests.get(weatherbit_api_base + current_weather + '?lat='+lat+'&lon='+lon+'&lang=pt&days=7&key=' + weatherbit_api_key)
-    show = response.json()
-    return(show)
+    if request.headers.get('city'):
+        place = request.args('city')
+        response = requests.get(weatherbit_api_base + current_weather + '?city=' + place + '&lang=pt&days=7&key=' + weatherbit_api_key)
+        show = response.json()
+        return(show)
+    else:
+        lat = request.args.get('lat')
+        lon = request.args.get('lon')
+        response = requests.get(weatherbit_api_base + current_weather + '?lat='+lat+'&lon='+lon+'&lang=pt&days=7&key=' + weatherbit_api_key)
+        show = response.json()
+        return(show)
 
 
 if __name__ == "__main__":
