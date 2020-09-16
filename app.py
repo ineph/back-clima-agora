e7e124ada0090c564d610c5df34d734ae0f9d746
lucas <lcs999999999@hotmail.com>
2020-09-16 16:50:42 -0300

    need to separate files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from cachetools import TTLCache
+
 import requests
 import json
+from cachetools import TTLCache
 from flask import Flask, request
 from flask_cors import CORS
 
@@ -9,74 +10,64 @@ CORS(app)
 
 weatherbit_api_base = 'http://api.weatherbit.io/v2.0/'
 weatherbit_api_key = '55122f2c18204337a2a823a4e91778fa'
-# forecast_weather = 'forecast/daily'
-# current_weather = 'current'
 
-route_forecast_cache = {}
-route_current_cache = {}
-cache = TTLCache(maxsize=80, ttl=600)
+cache_forecast = TTLCache(maxsize=80, ttl=600)
+cache_current = TTLCache(maxsize=80, ttl=600)
 
 
-@app.route("/<route>")
-def get_method(route):
-
+@app.route("/current")
+def get_current():
     place = request.args.get('city')
     lat = request.args.get('lat')
     lon = request.args.get('lon')
     coordinates = str(lat) + str(lon)
     str_coordinates = coordinates.replace('-', 'm')
-    str_place = str(place)
-
-    if route == 'forecast':
-        route = 'forecast/daily'
 
     if place:
-        if place in cache:
-            print('USED CACHE: place')
-            return(cache[str_place])
+        if place in cache_current:
+            print('used cache_current current place')
+            return(cache_current[place])
         else:
-            print('AAAAAAOOOOOOOOOOO')
-            print(route)
-            response = requests.get(weatherbit_api_base + route + '?city=' + place + '&lang=pt&days=7&key=' + weatherbit_api_key)
-            cache[str_place] = response.json()
+            response = requests.get(weatherbit_api_base + 'current' + '?city=' + place + '&lang=pt&days=7&key=' + weatherbit_api_key)
+            cache_current[place] = response.json()
             show = response.json()
-            print('NOT USED CACHE: place')
             return(show)
     else:
-        if str_coordinates in cache:
-            print('USED CACHE TO COORDINATES')
-            return(cache[str_coordinates])
+        if str_coordinates in cache_current:
+            print('used cache current coordinates')
+            return(cache_current[str_coordinates])
         else:
-            response = requests.get(weatherbit_api_base + route + '?lat=' + lat + '&lon=' + lon + '&lang=pt&days=7&key=' + weatherbit_api_key)
-            cache[str_coordinates] = response.json()
+            response = requests.get(weatherbit_api_base + 'current' + '?lat=' + lat + '&lon=' + lon + '&lang=pt&days=7&key=' + weatherbit_api_key)
+            cache_current[str_coordinates] = response.json()
             show = response.json()
-            print('NOT USED CACE TO CORDINATES')
             return(show)
 
-# @app.route("/current")
-# def get_current():
-#     place = request.args.get('city')
-#     lat = request.args.get('lat')
-#     lon = request.args.get('lon')
-#     coordinates = str(lat) + str(lon)
-#     str_coordinates = coordinates.replace('-', 'm')
+@app.route("/forecast")
+def get_forecast():
+    place = request.args.get('city')
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    coordinates = str(lat) + str(lon)
+    str_coordinates = coordinates.replace('-', 'm')
 
-#     if place:
-#         if place in cache:
-#             return(cache[place])
-#         else:
-#             response = requests.get(weatherbit_api_base + current_weather + '?city=' + place + '&lang=pt&days=7&key=' + weatherbit_api_key)
-#             cache[place] = response.json()
-#             show = response.json()
-#             return(show)
-#     else:
-#         if str_coordinates in cache:
-#             return(cache[str_coordinates])
-#         else:
-#             response = requests.get(weatherbit_api_base + current_weather + '?lat=' + lat + '&lon=' + lon + '&lang=pt&days=7&key=' + weatherbit_api_key)
-#             cache[str_coordinates] = response.json()
-#             show = response.json()
-#             return(show)
+    if place:
+        if place in cache_forecast:
+            print('used cache forecast place')
+            return(cache_forecast[place])
+        else:
+            response = requests.get(weatherbit_api_base + 'forecast/daily' + '?city=' + place + '&lang=pt&days=7&key=' + weatherbit_api_key)
+            cache_forecast[place] = response.json()
+            show = response.json()
+            return(show)
+    else:
+        if str_coordinates in cache_forecast:
+            print('used cache fprecast coords')
+            return(cache_forecast[str_coordinates])
+        else:
+            response = requests.get(weatherbit_api_base + 'forecast/daily' + '?lat=' + lat + '&lon=' + lon + '&lang=pt&days=7&key=' + weatherbit_api_key)
+            cache_forecast[str_coordinates] = response.json()
+            show = response.json()
+            return(show)
 
 if __name__ == "__main__":
     app.run()
